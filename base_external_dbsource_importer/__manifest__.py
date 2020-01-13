aee6e8e9e2c0a0d9b2e03a0adcc39aa57a12ae76
Sergio Teruel <sergio.teruel@tecnativa.com>
2020-01-13 09:41:22 +0100

    [MIG] base_external_dbsource_importer: Migration to v13.0

--- base_external_dbsource_importer/__manifest__.py
+++ base_external_dbsource_importer/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Base External Dbsource Importer",
     "summary": "Import data from external DB Sources",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "development_status": "Alpha",
     "category": "Tools",
     "website": "https://github.com/OCA/server-backend",
